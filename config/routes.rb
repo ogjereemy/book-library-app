a093e3325f530ae862e92ebe111fa52475458507
ogola jeremy <ogolajeremy601@gmail.com>
2025-03-01 07:52:20 +0300

    feat: Enhance book borrowing and returning logic, add user profile, and improve styling
    - Add returned_at validation and returned? method to Borrowing model
    - Update BorrowingsController to handle book availability status on borrow and return
    - Add user profile route and controller action
    - Ensure Devise routes are defined before custom routes to avoid conflicts
    - Use form_with for sign out link to handle DELETE method properly
    - Enhance styling for application layout
    - Add styling to books index page
    - Add styling to books show page

--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,7 @@ Rails.application.routes.draw do
   resources :borrowings, only: [:index, :create, :update, :destroy]
   
   get 'user_profile/:user_id', to: 'users#profile', as: 'user_profile'
-
-  # get '/users/sign_out', to: redirect('/')
-
+  
   get 'user/:user_id/borrowings', to: 'borrowings#index', as: 'user_borrowings'
   
   root 'books#index'
