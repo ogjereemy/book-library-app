d8eafff57df1eb234031ace04ee7f65d20c523ee
ogola jeremy <ogolajeremy601@gmail.com>
2025-03-02 10:58:34 +0300

    debbuging

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users
-
-  resources :users, only: [ :show ]
-
+  
+  resources :users, only: [:show]
+  
   resources :books do
     member do
-      post "borrow"
-      delete "return_book", to: "books#return_book"
-      post "return_book"
-      patch "return_book"
+      post 'borrow'
+      delete 'return_book'
     end
   end
-
-  resources :borrowings, only: [ :index, :create, :update, :destroy ]
-  resources :borrowings do
+  
+  resources :borrowings, only: [:index, :create, :update, :destroy] do
     member do
-      delete "return", to: "borrowings#return_book"
+      delete 'return_book', to: 'borrowings#return_book'
+      get 'return_book'
     end
   end
-
-
-
-  get "user_profile/:user_id", to: "users#profile", as: "user_profile"
-
-  get "user/:user_id/borrowings", to: "borrowings#index", as: "user_borrowings"
-
-  root "books#index"
+  
+  get 'user_profile/:user_id', to: 'users#profile', as: 'user_profile'
+  get 'user/:user_id/borrowings', to: 'borrowings#index', as: 'user_borrowings'
+  
+  root 'books#index'
 end
